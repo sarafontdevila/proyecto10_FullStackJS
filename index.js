853fbf4ab18442cc21c43875f17012235cff2a38
sarafontdevila <146539364+sarafontdevila@users.noreply.github.com>
2025-04-10 23:01:08 +0200

    files

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+require ("dotenv").config()
 const express = require ('express');
 
 const app = express()
 
-app.use("/ping", (req, res, next) => {})
-
-app.use ("*", (req, res, next) => {
-  return res.status(404).json("Route Not Found")
+app.use("/ping", (req, res, next) => {
+  res.status(200).json("Pong!");
 })
 
-
 app.listen(3000, () => {
-  console.log("http://localhost:3000")
+  console.log('Escuchando en http://localhost:3000')
 })
