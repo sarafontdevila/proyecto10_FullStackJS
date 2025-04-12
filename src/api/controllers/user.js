1e12f56a7144b46fa70c751c3904de2e76eb213a
sarafontdevila <146539364+sarafontdevila@users.noreply.github.com>
2025-04-12 17:13:20 +0200

    users 1

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -1,45 +1,46 @@
-const User = require("../models/user")
-const bcrypt = require ("bcrypt")
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 const getUsers = async (req, res, next) => {
   try {
     const users = await User.find()
     return res.status(200).json(users)
   } catch (error) {
-    return res.status(400).json("error")
-    
+    return res.status(400).json('error')
   }
 }
 const getUserById = async (req, res, next) => {
   try {
-    
+    const { id } = req.params
+    const user = await User.findById(id)
+    return res.status(200).json(user)
   } catch (error) {
-    return res.status(400).json("error")
-    
+    return res.status(400).json('error')
   }
 }
 const register = async (req, res, next) => {
   try {
-    
+    const userduplicated = await User.findOne({ email: req.body.email })
+    if (userduplicated) {
+      return res.status(400).json('Usuario ya existe')
+    }
+    const newUser = new User(req.body)
+    const user = await newUser.save()
+    return res.status(200).json(user)
   } catch (error) {
-    return res.status(400).json("error")
-    
+    return res.status(400).json('error')
   }
 }
-const login= async (req, res, next) => {
+const login = async (req, res, next) => {
   try {
-    
   } catch (error) {
-    return res.status(400).json("error")
-    
+    return res.status(400).json('error')
   }
 }
 const updateUser = async (req, res, next) => {
   try {
-    
   } catch (error) {
-    return res.status(400).json("error")
-    
+    return res.status(400).json('error')
   }
 }
 
--- src/api/models/user.js
+++ src/api/models/user.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 const bcrypt = require ("bcrypt")
 
 const userSchema = new mongoose.Schema({
-  email: {type: String, required: true},
-  password: {type: Number, required: true},
+  email: {type: String, trim: true, required: true},
+  password: {type: String, required: true},
   preferidos: [{type: mongoose.Types.ObjectId, required:false, ref: "eventos"}
   ],
 },
